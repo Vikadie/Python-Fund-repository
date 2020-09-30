58115fa75f8f2f5a0bf85fd2f9a2151e45551a3c
Vikadie <68245263+Vikadie@users.noreply.github.com>
2020-09-30 14:02:10 +0300

    Python Advanced 4th commit

--- hackerrank/re.group.py
+++ hackerrank/re.group.py
@@ -5,6 +5,10 @@ inp = input()
 pattern = r'([a-zA-Z0-9])\1'
 m = re.search(pattern, inp)
 
-print(m.group(1))
-print(m.group(0))
-print(m.groups())
+if m:
+    print(m.group())
+    print(m.group(1))
+    print(m.group(0))
+    print(m.groups())
+else:
+    print(-1)
